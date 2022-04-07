36f3875b727ec39107ecb0473ca789b850a1ce9d
90daniel <d_mallcu@hotmail.com>
2022-04-07 19:10:01 -0300

    router videogame get ID

--- api/src/routes/videogame.js
+++ api/src/routes/videogame.js
@@ -1,7 +1,17 @@
 const { Router } = require('express');
 require('dotenv').config();
 const { videogame } = require('../controllers')
-const {Videogame, Genre} = require('../db.js')
+const { Videogame, Genre } = require('../db.js');
+const router = require('./genres');
 
 const Router = Router();
 
+router.get('/:idVideogame', async (req, res, next) => {
+    const { idVideogame } = req.params //ID received by params
+    let data = await videogame(idVideogame)
+    try {
+        data ? res.send(data) : res.status(404).send('El id ingresado no coincide con un videojuego en particular')
+    } catch (e) {
+        next(e)
+    }
+})
