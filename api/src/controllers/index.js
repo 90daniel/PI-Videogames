28cfe42617afd0d1288f810503536b8a0852977d
90daniel <d_mallcu@hotmail.com>
2022-04-07 18:48:11 -0300

    request to API and DB

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,12 +1,12 @@
 const { default: axios } = require('axios');
-const {Videogame, Genre} = require('../db.js')
+const { Videogame, Genre } = require('../db.js')
 
 //Get 100 videogames from API
-const infoApi = async() => {
+const infoApi = async () => {
     let url = `https://api.rawg.io/api/games?key=f9ee51ed795746a1970f93399a92c096`
     let videogames = []
     try {
-        for(let i=0; i<5; i++) {    //5 times because it's an array of 20 videogames and we need 100
+        for (let i = 0; i < 5; i++) {    //5 times because it's an array of 20 videogames and we need 100
             const result = await axios.get(url)     //request to API. This has a .data property with an array of videogames and a .next property with the next page
             result.data.results.map(e => {      //map videogames from array of videogames
                 videogames.push({            //push videogames in videogames array
@@ -22,7 +22,7 @@ const infoApi = async() => {
             url = result.data.next
         }
         return videogames
-    } catch(e) {
+    } catch (e) {
         console.log(e)
     }
 };
@@ -39,17 +39,81 @@ const infoDB = async () => {
                 }
             }]
         })
-    } catch(e) {
+    } catch (e) {
         console.error(e)
     }
 }
 
-//Get videogames from API and DB
+//Get total videogames from API and DB
 const infoTotal = async () => {
     //to join the two requests, save the execution of the functions
-    const apiData = await infoApi ();
+    const apiData = await infoApi();
     const dbData = await infoDB();
     //now join the two arrays
     const infoCompleta = dbData.concat(apiData)
     return infoCompleta
+}
+
+//Request to API. GET https://api.rawg.io/api/games?search={name}
+const nameApi = async (name) => {
+    const infoSearch = await axios.get(`https://api.rawg.io/api/games?search=${name}&key=f9ee51ed795746a1970f93399a92c096`)
+    //console.log(infoSearch.data.results). This is the array of videogames that match the name searched. We need to map it to get the videogames that match the name searched and return it as an array of videogames.
+    try {
+        const vgSearch = await infoSearch.data.results.map(vg => {
+            return {
+                id: vg.id,
+                name: vg.name,
+                image: vg.background_image,
+                rating: vg.rating,
+                platforms: vg.platforms?.map(el => el.platform.name),
+                genres: vg.genres?.map(el => el.name)
+            }
+        })
+        return vgSearch;
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
+//Request to API. Get https://api.rawg.io/api/games/{id}
+const idApi = async (id) => {
+    try {
+        const rtaApi = await axios.get(`https://api.rawg.io/api/games/${id}?key=f9ee51ed795746a1970f93399a92c096`)
+        if (rtaApi) {
+            const vgId = await rtaApi.data
+            const info = {
+                id: vgId.id,
+                name: vgId.name,
+                image: vgId.background_image,
+                genre: vgId.genres?.map(el => el.name),
+                description: vgId.description,
+                released: vgId.released,
+                rating: vgId.rating,
+                platforms: vgId.platforms?.map(el => el.platform.name),
+            }
+            return info
+        } else {
+            return ("No se encontró el juego con ese ID")
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+//Request to DB
+const idDb = async (id) => {
+    try {
+        return await Videogame.findByPk(id, {
+            include: [{
+                model: Genre,
+                atributes: ['name'],
+                throught: {
+                    attributes: []
+                }
+            }]
+        })        
+    } catch(e) {
+        console.error(e)
+    }
 }
